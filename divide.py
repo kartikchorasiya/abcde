1a4058a04ae58498463892518ee9b4bfd7da4079
Yugal Sharma <yugalsharma13@gmail.com>
2018-10-16 22:40:56 +0530

    Add files via upload

---
+++ divide.py
@@ -0,0 +1,21 @@
+def hello_world():
+    print("Hello world hoe r u")
+
+
+def add(a,b):
+   # program to add two numbers
+   print(a+b)
+
+
+def subtract(a,b):
+   # subtract two numbers
+   print(a-b)
+   
+   
+def multiply(a,b):
+     # program to multiply
+     print(a*b)
+	 
+	 
+def divide(a,b):
+    print(a/b)
