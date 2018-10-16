6908c8aa3624f79d2ac9ef88c9c61c3a22652eb4
Yugal Sharma <yugalsharma13@gmail.com>
2018-10-16 22:39:30 +0530

    Add files via upload

---
+++ multiply.py
@@ -0,0 +1,17 @@
+def hello_world():
+    print("Hello world hoe r u")
+
+
+def add(a,b):
+   # program to add two numbers
+   print(a+b)
+
+
+def subtract(a,b):
+   # subtract two numbers
+   print(a-b)
+   
+   
+def multiply(a,b):
+     # program to multiply
+     print(a*b)
